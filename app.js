182a979c08769e9f71e69a2621605ee289a74fb9
Leo Lozach <leo.lozach@gmail.com>
2017-10-31 19:54:58 +0100

    Add optional exit timer with env var SERVE_DELAY

--- app.js
+++ app.js
@@ -23,8 +23,9 @@ Object.keys(options).forEach(function (key) {
 })
 args.push(dir)
 
+process.stdout.write('node ' + args.join(' ') + '\n')
+
 const child = spawn('node', args)
-console.log('node ' + args.join(' '))
 
 child.stdout.on('data', function (data) {
   process.stdout(String(data))
@@ -33,3 +34,16 @@ child.stdout.on('data', function (data) {
 child.stderr.on('data', function (data) {
   throw new Error(String(data))
 })
+
+// stop timer
+if (process.env.SERVE_DELAY) {
+  var delay = parseInt(process.env.SERVE_DELAY)
+  delay = isNaN(delay) ? 0 : Math.max(0, delay)
+
+  if (delay > 0) {
+    console.log('Serve will stop after ' + delay + 'msec')
+    setTimeout(function () {
+      child.kill('SIGHUP')
+    }, delay)
+  }
+}
